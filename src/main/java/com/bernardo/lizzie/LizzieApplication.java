9c3b276fc397be25a43f742904618c78943be0aa
Chimmy2302 <98993193+Chimmy2302@users.noreply.github.com>
2025-11-04 00:15:34 +0800

    initializing the project

---
+++ src/main/java/com/bernardo/lizzie/LizzieApplication.java
@@ -0,0 +1,13 @@
+package com.bernardo.lizzie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LizzieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LizzieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bernardo/lizzie/LizzieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bernardo.lizzie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LizzieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
